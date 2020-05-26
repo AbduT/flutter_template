56435fd63b350f1b564b0d4f3431c2af607e7f68
AbduT <32500550+AbduT@users.noreply.github.com>
2020-05-26 18:42:43 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import './shared/providers/name_notifier.dart';
+import './routes.dart';
+import './modules/auth/auth_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => ClassName()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        routes: Routes.routes,
+        home: AuthScreen(),
+      ),
+    );
+  }
+}
